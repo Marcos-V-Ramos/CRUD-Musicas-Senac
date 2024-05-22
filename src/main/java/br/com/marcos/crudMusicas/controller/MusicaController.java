5bb01be9a554cef281621a606f7958e3a75c2a7a
Marcos V Ramos <viniciusramoscaldas@gmail.com>
2024-05-22 19:12:04 -0300

    fix: use service layer and expose endpoints

--- src/main/java/br/com/marcos/crudMusicas/controller/MusicaController.java
+++ src/main/java/br/com/marcos/crudMusicas/controller/MusicaController.java
@@ -1,48 +1,41 @@
 package br.com.marcos.crudMusicas.controller;
 
 import br.com.marcos.crudMusicas.dto.MusicaDTO;
-import org.springframework.http.HttpStatus;
+import br.com.marcos.crudMusicas.service.MusicaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/musicas")
 public class MusicaController {
 
-    private MusicaDTO musicaDTO = new MusicaDTO();
+    @Autowired
+    private MusicaService musicaService;
+
     @GetMapping("/{id}")
     public ResponseEntity<MusicaDTO> findMusicaById(@PathVariable(value = "id") Long id) {
-        musicaDTO.setId(id);
-        return ResponseEntity.ok(musicaDTO);
+        return musicaService.mostrarMusica(id);
     }
 
     @GetMapping
     public ResponseEntity<List<MusicaDTO>> findAllMusics() {
-        List<MusicaDTO> musicas = new ArrayList<>();
-
-        musicas.add(new MusicaDTO(2L, "teste", "teste", "2000"));
-        musicas.add(new MusicaDTO(2L, "teste", "teste", "2000"));
-        musicas.add(new MusicaDTO(2L, "teste", "teste", "2000"));
-
-        return ResponseEntity.ok().body(musicas);
+        return musicaService.mostrarMusicas();
     }
 
     @PostMapping
     public ResponseEntity<MusicaDTO> createNewMusica(@RequestBody MusicaDTO musicaDTO) {
-        musicaDTO.setId(666L);
-        return ResponseEntity.status(HttpStatus.CREATED).body(musicaDTO);
+        return musicaService.salvarMusica(musicaDTO);
     }
 
     @PutMapping
     public ResponseEntity<MusicaDTO> updateMusica(@RequestBody MusicaDTO musicaDTO) {
-        musicaDTO.setTitulo("atualizaduuu");
-        return ResponseEntity.status(HttpStatus.OK).body(musicaDTO);
+        return musicaService.alterarMusica(musicaDTO);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMusica(@PathVariable(value = "id") Long id) {
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return musicaService.removerMusica(id);
     }
 }
